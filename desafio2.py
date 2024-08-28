9edc674e973e4acce155e8663671e60b47d1e2c1
Antonio12022 <antoniofeliz884@gmail.com>
2024-08-28 19:44:39 -0300

    flamengo commit

---
+++ desafio2.py
@@ -0,0 +1,11 @@
+#numero_de_horas=200
+#valor_por_horas = 35
+#salario = numero_de_horas*valor_por_horas
+#print ('seu salario ao final do mes é ',salario)
+#nome = (input('Digite o seu nome:\t'))
+#print ('A string tem : ',len(nome),'caracteres')
+altura = float (input("qual é sua altura"))
+peso = float (input('qual é seu peso' ))
+idade = int (input('quantos anos você'))
+ 
+print (altura, peso, idade)
